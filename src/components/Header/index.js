aa0000f4c13cb594b0eb3890f337ebd2824ccc57
slash585 <slsh585@gmail.com>
2022-02-28 02:00:15 +0300

    Setup context api and add to basket

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom"
 import SearchIcon from "@material-ui/icons/Search"
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
 import "./header.css"
+import { useStateValue } from "../../StateProvider"
 
 function Header() {
+  const [{ basket }] = useStateValue()
+
   return (
     <nav className="header">
       <Link to="/">
@@ -46,7 +49,9 @@ function Header() {
       <Link to="/checkout" className="headerLink">
         <div className="headerOptionBasket">
           <ShoppingBasketIcon />
-          <span className="headerOptionLineTwo headerBasketCount">0</span>
+          <span className="headerOptionLineTwo headerBasketCount">
+            {basket?.length}
+          </span>
         </div>
       </Link>
     </nav>
--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,7 +1,22 @@
 import React from "react"
 import "./product.css"
+import { useStateValue } from "../../StateProvider"
 
 function Product({ id, title, price, rating, image }) {
+  const [{ basket }, dispatch] = useStateValue()
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    })
+  }
+
   return (
     <div className="product">
       <div className="productInfo">
@@ -13,14 +28,14 @@ function Product({ id, title, price, rating, image }) {
         <div className="productRating">
           {Array(rating)
             .fill()
-            .map((_) => (
-              <p>⭐️</p>
+            .map((_, i) => (
+              <p key={i}>⭐️</p>
             ))}
         </div>
       </div>
 
       <img alt="" src={image} />
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   )
 }
